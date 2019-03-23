91b02602dfb03fde270106d57c319c035b4a7d77
david-chua <dp_chua@hotmail.com>
2019-03-23 12:36:14 -0400

    added dependencies, set up knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/project_tracker.db3'
+    },
+    useNullAsDefault: true,
+    migratiosn: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
